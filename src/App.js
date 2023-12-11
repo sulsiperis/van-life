f9b8d16da3ec8739fa7c3e14bda01b4c16b86bbd
Sulsiperis <fileopen@gmail.com>
2023-12-12 01:36:12 +0200

    Initial commit with some router basics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function About() {
+  return (
+    <h1>About page</h1>
+  )
+}
+
+function Home() {
+  return (
+    <div className="App">
+        <h1>Hello, haidoo, ca va</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to={'/'}>Home</Link> {/* Link is rendered into <a></a> tag */}
+        <Link to={'/about'}> About</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>    
   );
 }
 
