6e1fb4e913bc4fa91a25dc87458e12210a86b075
Sulsiperis <fileopen@gmail.com>
2023-12-12 15:18:52 +0200

    changel home btn into router link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import background from '../img/homeback.png'
-
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
     <div className='home-wrapper' style={{background: `url(${background})`}}>
         <h2>You got the travel plans, we got the travel vans.</h2>
         <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
-        <button>Find your van</button>
+        <Link to={"/vans"} className='link-btn'>Find your van</Link>
+        
     </div>
   )
 }
