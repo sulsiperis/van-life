2f6835180ebb6333931874ac966c9b5df4b209aa
Sulsiperis <fileopen@gmail.com>
2023-12-12 04:00:19 +0200

    Pages home, about, vans routing. Home & about fully styled css

--- src/App.js
+++ src/App.js
@@ -1,33 +1,51 @@
 
 import './App.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Vans from './pages/Vans';
 
-function About() {
-  return (
-    <h1>About page</h1>
-  )
-}
+/**
+ * Challenge:
+ * Bootstrap the VanLife project by creating the first 2 routes:
+ * Home and About.
+ * 
+ * Also include the navbar that can link between the two routes.
+ * For now, you'll either need to copy/paste the navbar code
+ * to both Home and About pages, or you'll need to find a place
+ * to put it where it can be shared between the two pages.
+ * (Don't overthink this part - just do whatever is easiest for
+ * you because we'll learn a better approach very soon)
+ * 
+ * Review challenge: do all the CSS yourself based on the design
+ * linked in the slides.
+ */
 
-function Home() {
-  return (
-    <div className="App">
-        <h1>Hello, haidoo, ca va</h1>
-    </div>
-  )
-}
+
 
 function App() {
   return (
-    <BrowserRouter>
-      <nav>
-        <Link to={'/'}>Home</Link> {/* Link is rendered into <a></a> tag */}
-        <Link to={'/about'}> About</Link>
-      </nav>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-      </Routes>
-    </BrowserRouter>    
+    <div className='main'>
+      <BrowserRouter>
+        <nav>
+          <Link className='logo' to={'/'}>#VANLIFE</Link> {/* Link is rendered into <a></a> tag */}
+          <div className='menu'>
+            <Link to={'/about'}> About</Link>
+            <Link to={'/Vans'}> Vans</Link>
+          </div>
+        </nav>
+        <div className='content'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/vans' element={<Vans />} />
+          </Routes>
+        </div>
+        <foot>
+          <h4>Ⓒ 2022 #VANLIFE</h4>
+        </foot>
+      </BrowserRouter>
+    </div>    
   );
 }
 
---
+++ src/pages/About.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import background from '../img/aboutback.png'
+
+function About() {
+  return (
+    <>    
+    <div style={{
+        background: `url(${background})`,
+        height: '233px',
+        width: '550px',
+        backgroundPosition: '50%',
+        backgroundRepeat: 'no-repeat'
+
+    }}></div>
+    <div className='about-wrapper'>
+        
+        <h2>You got the travel plans, we got the travel vans.</h2>
+        <p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.
+(Hitch costs extra 😉)</p>
+        <p>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
+        <div className='about-destination'>
+            <h3>Your destination is waiting. Your van is ready.</h3>
+            <button>Explore our vans</button>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default About
