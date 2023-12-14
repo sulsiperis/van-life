ccc5739a6eb346ecab82c4d0c0f0e81d3b3dd087
Sulsiperis <fileopen@gmail.com>
2023-12-14 12:14:41 +0200

    styling active routes with <NavLink>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,22 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink, Link } from 'react-router-dom'
 
 function Header() {
+    const activeStyle = {
+        fontWeight: "700",
+        color: "red"
+    }
   return (
     <nav>
         <Link className='logo' to={'/'}>#VANLIFE</Link> {/* Link is rendered into <a></a> tag */}
         <div className='menu'>
-            <Link to={'/host'}>Host</Link>
-            <Link to={'/about'}>About</Link>
-            <Link to={'/vans'}>Vans</Link>
+            <NavLink 
+                className={({isActive}) => isActive ? 'header-menu-active' : ''} /* first method for styling active route */
+                //style={({isActive}) => isActive ? activeStyle : null } /* second method */
+                to={'/host'}
+            >Host</NavLink>
+            <NavLink className={({isActive}) => isActive ? 'header-menu-active' : ''} to={'/about'}>About</NavLink>
+            <NavLink className={({isActive}) => isActive ? 'header-menu-active' : ''} to={'/vans'}>Vans</NavLink>
         </div>
     </nav>
   )
