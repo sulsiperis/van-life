b624101c28ece1b4d4a9efc886a7aba5a487f8fe
Sulsiperis <fileopen@gmail.com>
2023-12-23 13:20:24 +0200

    defer, <Await> and React.Suspense implementation in hostVans

--- src/pages/host/HostVans.js
+++ src/pages/host/HostVans.js
@@ -1,32 +1,45 @@
 import React from 'react'
-import { Link, useLoaderData } from 'react-router-dom'
+import { Link, useLoaderData, defer, Await } from 'react-router-dom'
 import { getHostVans } from '../../api'
 import { requireAuth } from '../../utils'
 
 export async function loader({ request }) {
     await requireAuth(request)
-    return getHostVans()
+    return defer({ vans: getHostVans() })
 }
 
 function HostVans() {
-    const vans = useLoaderData()
-    const vanList = vans.map(van => (
-        <Link key={van.id} to={van.id}>
-            <div className='host-van-item'>
-                <img src={van.imageUrl} />
-                <div>
-                    <h4>{van.name}</h4>
-                    <p>€{van.price}/day</p>
+    const dataPromise = useLoaderData()
+    
+    function renderHostVansElements(vans) {
+        const vanList = vans.map(van => (
+            <Link key={van.id} to={van.id}>
+                <div className='host-van-item'>
+                    <img src={van.imageUrl} />
+                    <div>
+                        <h4>{van.name}</h4>
+                        <p>€{van.price}/day</p>
+                    </div>
                 </div>
-            </div>
-        </Link>
-    ))
+            </Link>
+        ))
+        return (
+            <>
+                <h1>Your listed vans</h1>
+                {vanList} 
+            </>
+        )
+
+    }
     
 
   return (
     <div className='host-vans-wrapper'>
-        <h1>Your listed vans</h1>
-        {vanList}        
+        <React.Suspense fallback={<h2>Loading...</h2>}>
+            <Await resolve={dataPromise.vans}>
+                {renderHostVansElements}
+            </Await>
+        </React.Suspense>
     </div>
   )
 }
