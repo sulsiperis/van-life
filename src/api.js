153fa20f4ed5b81886d0f1241163028a862dfb84
Sulsiperis <fileopen@gmail.com>
2023-12-25 01:25:58 +0200

    Firebase setup. Server mirage.js passthrough google. api.js restructuring to fetch data from firebase. getVans implementation with firebase db.

--- src/api.js
+++ src/api.js
@@ -1,4 +1,34 @@
-export async function getVans(id) {
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDocs, getFirestore, collection } from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyByX7UtmOzfLnJn8KpmL_J3mHvqpFTowx0",
+  authDomain: "van-life-add98.firebaseapp.com",
+  projectId: "van-life-add98",
+  storageBucket: "van-life-add98.appspot.com",
+  messagingSenderId: "52160060792",
+  appId: "1:52160060792:web:7be7981bedd02789bf7c8d",
+  measurementId: "G-E654FC0KF8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getFirestore(app); //reference needed for delete note func
+const vansCollectionRef = collection(db, "vans")
+
+export async function getVans() {
+    const querySnapshot = await getDocs(vansCollectionRef)
+    const dataArr = querySnapshot.docs.map(doc => ({
+        ...doc.data()
+    }))
+   // console.log(dataArr[0].id)
+    return dataArr
+}
+
+/* export async function getVans(id) {
   const url = id ? `/api/vans/${id}` : "/api/vans"
   const res = await fetch(url)
   if (!res.ok) {
@@ -10,7 +40,7 @@ export async function getVans(id) {
   }
   const data = await res.json()
   return data.vans
-}
+} */
 
 export async function getHostVans(id) {
   const url = id ? `/api/host/vans/${id}` : "/api/host/vans"
--- src/server.js
+++ src/server.js
@@ -20,6 +20,7 @@ createServer({
     routes() {
         this.namespace = "api"
         this.logging = false
+        this.passthrough("https://firestore.googleapis.com/**")
         // this.timing = 2000
 
         this.get("/vans", (schema, request) => {
